292b844beed7139ed713ff1e8db98d412f9b6e7e
Lingyu Zhou <zly@debianexplorer.local>
2019-08-30 00:08:36 -0400

    modified Med..Orders.js

--- src/components/resources/MedicationOrder.js
+++ src/components/resources/MedicationOrder.js
@@ -1,20 +1,8 @@
 import React from 'react'
 var _ = require('lodash');
 import ResourceContainer from '../container/ResourceContainer'
+import Reference from '../datatypes/Reference'
 import crypto from 'crypto'
-
-class MedicationDetails extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div>
-       <h4>{this.props.medication} </h4>
-      </div>
-    )
-  }
-}
 
 class MedicationOrder extends React.Component {
   constructor(props) {
@@ -23,27 +11,92 @@ class MedicationOrder extends React.Component {
 
   render() {
     return (
-      <div className='col-xs-8'>
+      <div>
         <ResourceContainer {...this.props} >
-          <div >
-            <MedicationDetails medication={_.get(this.props.fhirResource, 'medicationReference.display') || _.get(this.props.fhirResource, 'medicationCodeableConcept.text')}/>
+          <div style={{width:'100%', display:'inline-block'}}>
+            <h4 style={{display: 'inline-block'}}>
+              {_.get(this.props.fhirResource,'medicationReference.display') || _.get(this.props.fhirResource,'medicationCodeableConcept.text') || ''}
+            </h4>
+            &nbsp;({_.get(this.props.fhirResource,'status') || ''})
           </div>
-          <div>
-             {this.props.fhirResource.reasonCodeableConcept && this.props.fhirResource.reasonCodeableConcept.coding ? <div> <b>Reason:</b> {_.get(this.props.fhirResource.reasonCodeableConcept.coding[0], 'display')} </div> : ''}
-          </div>
-          <div>
-             {this.props.fhirResource.dosageInstruction && this.props.fhirResource.dosageInstruction.length > 0 ? <div> <b>Dosage Instruction:</b> {_.get(this.props.fhirResource.dosageInstruction[0], 'text')} </div> : ''}
-          </div>
-          <div>
-             {this.props.fhirResource.dosageInstruction && this.props.fhirResource.dosageInstruction.length > 0 && this.props.fhirResource.dosageInstruction[0].additionalInstructions > 0 ? <div> <b>Additional Information:</b> {_.get(this.props.fhirResource.dosageInstruction[0].additionalInstructions, 'text')} </div> : ''}
-          </div>
-          <div>
-             {this.props.fhirResource.dosageInstruction && this.props.fhirResource.dosageInstruction.length > 0 && this.props.fhirResource.dosageInstruction[0].additionalInstructions ? <div> <b>Additional Information:</b> {_.get(this.props.fhirResource.dosageInstruction[0].additionalInstructions, 'text')} </div> : ''}
-          </div>
-          <div>
-            <div>
-             {this.props.fhirResource.dosageInstruction && this.props.fhirResource.dosageInstruction.length > 0 && this.props.fhirResource.dosageInstruction[0].route ? <div> <b>Route:</b> {_.get(this.props.fhirResource.dosageInstruction[0].route.coding[0], 'display')} </div> : ''}
-          </div>
+          <div className='container'>
+            <div className='row'>
+              <div className='col-sm-6'>
+                {this.props.fhirResource.dosageInstruction && this.props.fhirResource.dosageInstruction.length > 0  ? (
+                  <div>
+                    <small className='text-muted text-uppercase'><strong>
+                    Dosage Instruction:
+                    </strong></small>
+                    {_.get(this.props.fhirResource.dosageInstruction[0], 'text')}
+                  </div>) :
+                ''}
+              </div>
+              <div className='col-sm-6'>
+                {this.props.fhirResource.dosageInstruction && this.props.fhirResource.dosageInstruction.length > 0 &&
+                  this.props.fhirResource.dosageInstruction[0].route ? (
+                  <div>
+                    <small className='text-muted text-uppercase'><strong>
+                    Route:
+                    </strong></small>
+                    {_.get(this.props.fhirResource.dosageInstruction[0].route.coding[0], 'display')}
+                  </div>) :
+                ''}
+              </div>
+            </div>
+            
+            <div className='row'>
+              <div className='col-sm-4'>
+                {this.props.fhirResource.dispenseRequest && this.props.fhirResource.dispenseRequest.validityPeriod &&
+                 this.props.fhirResource.dispenseRequest.validityPeriod.start ? (
+                  <div>
+                    <small className='text-muted text-uppercase'><strong>
+                    Start time:
+                    </strong></small>
+                    {this.props.fhirResource.dispenseRequest.validityPeriod.start}
+                  </div>) :
+                ''}
+              </div>
+              <div className='col-sm-4'>
+                {this.props.fhirResource.dispenseRequest && this.props.fhirResource.dispenseRequest.validityPeriod &&
+                 this.props.fhirResource.dispenseRequest.validityPeriod.end ? (
+                  <div>
+                    <small className='text-muted text-uppercase'><strong>
+                    End time:
+                    </strong></small>
+                    {this.props.fhirResource.dispenseRequest.validityPeriod.end}
+                  </div>) : (
+                  <div>
+                    <small className='text-muted text-uppercase'><strong>
+                    End time:
+                    </strong></small>
+                    On-going
+                  </div>
+                )}
+              </div>
+              <div className='col-sm-4'>
+                {this.props.fhirResource.dateWritten ? (
+                  <div>
+                    <small className='text-muted text-uppercase'><strong>
+                    Date recorded:
+                    </strong></small>
+                    {this.props.fhirResource.dateWritten}
+                  </div>) :
+                ''}
+              </div>
+            </div>
+            
+            <div className='row'>
+              <div className='col-sm-12'>
+                {_.get(this.props.fhirResource, 'prescriber') ? (
+                  <div>
+                    <small className='text-muted text-uppercase'><strong>
+                    Prescribed by:
+                    </strong></small>
+                    {<Reference fhirData={_.get(this.props.fhirResource, 'prescriber')}/> || ''}
+                  </div>) :
+                ''}
+              </div>
+            </div>
           </div>
         </ResourceContainer>
       </div>
